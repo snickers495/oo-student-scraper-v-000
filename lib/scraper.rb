6f3afcd2cf4e7302993d419ff9f5ace49739ea05
Lisa Lee <snickers495@gmail.com>
2017-10-15 19:47:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,14 +24,14 @@ class Scraper
     profile = {}
     links = new_html.css('div.vitals-container div.social-icon-container a')
     links.each do |link|
-      if link.attr("href").text.include?("twitter")
-        profile[:twitter] = links.css("a").attr("href").text
-      elsif link.attr("href").text.include?("linkedin")
-        profile[:linkedin] = links.css("a").attr("href").text
-      elsif link.attr("href").text.include?("github")
-        profile[:github] = links.css("a").attr("href").text
-      elsif link.attr("href").text.include?("blog")
-        profile[:blog] = links.css("a").attr("href").text
+      if link.attr("href").value.include?("twitter")
+        profile[:twitter] = links.css("a").attr("href").value
+      elsif link.attr("href").value.include?("linkedin")
+        profile[:linkedin] = links.css("a").attr("href").value
+      elsif link.attr("href").value.include?("github")
+        profile[:github] = links.css("a").attr("href").value
+      elsif link.attr("href").value.include?("blog")
+        profile[:blog] = links.css("a").attr("href").value
       end
     end
     profile[:profile_quote] = new_html.css('div.vitals-container div.vitals-text-container div.profile-quote').text
